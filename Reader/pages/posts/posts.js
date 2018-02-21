573649f7da2807a958a5372e3080ca1c30e1ed67
zzm <zzm@rnsrec.com>
2018-02-22 05:50:24 +0800

    文件提交

--- Reader/pages/posts/posts.js
+++ Reader/pages/posts/posts.js
@@ -1,66 +1,29 @@
-// pages/posts/posts.js
+var postsData = require('../../data/posts-data.js')
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-  
-  },
+    // 小程序总会读取data 对象来做数据绑定，这个是在onLoad 事件执行之后发生的
 
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    this.setData({
+      postList: postsData.postList
+    });
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
+  onPostTap: function (event){
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
+    var postId = event.currentTarget.dataset.postid;
+    console.log("postId is :" + postId);
+    wx.navigateTo({
+      url: 'post-detail/post-detail',
+    })
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
   }
+  
 })
